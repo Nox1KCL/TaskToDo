d6d4216c273b1483fc2268a1443676721d8cdc3a
Nox1KCL <korshunov.maks09@gmail.com>
2026-01-09 15:11:38 +0200

    refactor: add type hint and case _: in mainloop

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import json
+from typing import Callable, Any
 
 
-def decorated_list(input_func):
-    def output_func(self):
-        print('-' * 20)
+def decorated_list(input_func: Callable[..., Any]) -> Callable[..., Any]:
+    def output_func(self: Any) -> None:
+        print("-" * 20)
         input_func(self)
-        print('-' * 20)
+        print("-" * 20)
 
     return output_func
 
 
 class Task:
-    def __init__(self, task_id, title, description, status=False):
+    def __init__(
+        self, task_id: int, title: str, description: str, status: bool = False
+    ):
         self.id = task_id
         self.title = title
         self.description = description
@@ -19,33 +22,38 @@ class Task:
 
 
 class ToDoList:
-    def __init__(self, file_name):
-        self.tasks = []
+    def __init__(self, file_name: str):
+        self.tasks: list[Task] = []
         self.load_from_json(file_name)
-        self.completed_tasks = []
+        self.completed_tasks: list[Task] = []
 
-
-    def save_to_json(self, file_name):
+    def save_to_json(self, file_name: str) -> None:
         data_to_save = [t.__dict__ for t in self.tasks]
-        with open(file_name, 'w', encoding='utf-8') as file:
+        with open(file_name, "w", encoding="utf-8") as file:
             json.dump(data_to_save, file, ensure_ascii=False)
 
-    def load_from_json(self, file_name):
+    def load_from_json(self, file_name: str) -> None:
         try:
-            with open(file_name, 'r', encoding='utf-8') as file:
+            with open(file_name, "r", encoding="utf-8") as file:
                 tasks = json.load(file)
                 for item in tasks:
-                    self.tasks.append(Task(item['id'], item['title'], item['description'], item['status']))
+                    self.tasks.append(
+                        Task(
+                            item["id"],
+                            item["title"],
+                            item["description"],
+                            item["status"],
+                        )
+                    )
         except (FileNotFoundError, json.JSONDecodeError):
             print("File not found.. create new one list")
             self.tasks = []
 
-    def id_reload(self):
+    def id_reload(self) -> None:
         for index, t in enumerate(self.tasks, start=1):
             t.id = index
 
-
-    def status_check(self):
+    def status_check(self) -> None:
         completed = [t for t in self.tasks if t.status]
 
         if not completed:
@@ -56,8 +64,7 @@ class ToDoList:
             self.completed_tasks.append(t)
             self.tasks.remove(t)
 
-
-    def mark_task(self, task_id):
+    def mark_task(self, task_id: int) -> None:
         for t in self.tasks:
             if t.id == task_id:
                 t.status = True
@@ -67,14 +74,12 @@ class ToDoList:
         else:
             print("No matches")
 
-
-    def add_task(self, title, description):
+    def add_task(self, title: str, description: str) -> None:
         new_id = len(self.tasks) + 1
         new_task = Task(new_id, title, description)
         self.tasks.append(new_task)
 
-
-    def del_task(self, task_id):
+    def del_task(self, task_id: int) -> None:
         for t in self.tasks:
             if t.id == task_id:
                 self.tasks.remove(t)
@@ -84,9 +89,8 @@ class ToDoList:
         else:
             print("No matches")
 
-
     @decorated_list
-    def show_tasks(self):
+    def show_tasks(self) -> None:
         if not self.tasks:
             print("Empty list")
         for t in self.tasks:
@@ -94,7 +98,7 @@ class ToDoList:
             print(f"{t.id}. {t.title} [{status}]")
 
     @decorated_list
-    def show_completed_tasks(self):
+    def show_completed_tasks(self) -> None:
         print("Here your completed task list:")
         if not self.completed_tasks:
             print("Empty list")
@@ -112,7 +116,9 @@ try:
         print("Here your current Tasks: ")
         my_list.show_tasks()
 
-        print("1 - mark task completed\n2 - add new task\n3 - delete task\n4 - view completed tasks\n5 - exit")
+        print(
+            "1 - mark task completed\n2 - add new task\n3 - delete task\n4 - view completed tasks\n5 - exit"
+        )
         which_option = int(input("Write what do you want to do: "))
         match which_option:
             case 1:
@@ -139,6 +145,8 @@ try:
             case 5:
                 print("Shutting down program..")
                 break
+            case _:
+                print("No matches")
 except KeyboardInterrupt:
     print("Detected some suddenly shutdown..\nSaving tasks..")
 finally:
